490c75ec8402426beff34b22695f10efa6c78692
James Barker <jeb.e.barker@gmail.com>
2020-09-26 17:17:23 -0400

    Fixed formatting errors

--- Barker_J_U1_L1.py
+++ Barker_J_U1_L1.py
@@ -1,7 +1,6 @@
 # Jeb Barker
 # Sep. 25, 2020
 import random
-from collections import deque
 
 
 def getInitialState():
@@ -14,22 +13,27 @@ def getInitialState():
 
 '''precondition: i<j
    swap characters at position i and j and return the new state'''
+
+
 def swap(state, i, j):
-    li = list(state) # create a list of each character in state
-    li[i], li[j] = li[j], li[i] # swap the elements of li at i/j
-    return "".join(s for s in li) # join every element in li into a string
+    li = list(state)  # create a list of each character in state
+    li[i], li[j] = li[j], li[i]  # swap the elements of li at i/j
+    return "".join(s for s in li)  # join every element in li into a string
 
 
 '''Generate a list which hold all children of the current state
    and return the list'''
+
+
 def generate_children(state):
-    sIndex = state.index("_") # store index of space (not necessary, just saves a little bit of space)
+    sIndex = state.index("_")  # store index of space (not necessary, just saves a little bit of space)
 
     return [swap(state, sIndex, j) for j, val in enumerate(list(state)) if not sIndex == j and (sIndex % 3 == j % 3 and abs(sIndex - j) <= 3) or ((sIndex - 1 == j and ((sIndex % 3) - 1 == j % 3)) or (sIndex + 1 == j and ((sIndex % 3) + 1 == j % 3)))]
-    # Return state after swapping space/j for every index in the given state. The following conditions are in order in the line above.
-    # Make sure the space and the index you are checking isn't the same
-    # Check if the space and the index you are ckecking are in the same column (Mod 3 produces this), only if the space and j are less than 4 spaces apart.
-    # Check of the character to the left/right of the space is in the same row using mod 3 +/- 1. E.X. space at index 2, j at index 3: 2%3 + 1 != 3%3.
+    # Return state after swapping space/j for every index in the given state. The following conditions are in order
+    # in the line above. Make sure the space and the index you are checking isn't the same Check if the space and the
+    # index you are checking are in the same column (Mod 3 produces this), only if the space and j are less than 4
+    # spaces apart. Check of the character to the left/right of the space is in the same row using mod 3 +/- 1. E.X.
+    # space at index 2, j at index 3: 2%3 + 1 != 3%3.
 
 
 def display_path(n, explored):  # key: current, value: parent
@@ -57,7 +61,7 @@ def display_path(n, explored):  # key: current, value: parent
 
 
 def BFS(initial_state):
-    explored = {initial_state:"s"}
+    explored = {initial_state: "s"}
     nodeQueue = [[initial_state]]
     while nodeQueue:
         path = nodeQueue.pop(0)
@@ -67,10 +71,8 @@ def BFS(initial_state):
         for state in generate_children(list(s)):
             if state not in explored:
                 nodeQueue.append([state])
-                explored[state] = s
+                explored[state] = s  # state is the child while s is the parent
                 # display_path(nodeQueue, explored)
-
-
 
 
 '''Find the shortest path to the goal state "_12345678" and
@@ -79,16 +81,16 @@ def BFS(initial_state):
 
 
 def DFS(initial):
-    explored = {initial:"s"}
-    nodeQueue = [[initial]]
-    while nodeQueue:
-        path = nodeQueue.pop()
+    explored = {initial: "s"}
+    nodeStack = [[initial]]
+    while nodeStack:
+        path = nodeStack.pop()
         s = path[-1]
         if s == "_12345678":
             display_path(s, explored)
         for state in generate_children(list(s)):
             if state not in explored:
-                nodeQueue.append([state])
+                nodeStack.append([state])
                 explored[state] = s
 
 
@@ -96,7 +98,7 @@ def main():
 
     initial = getInitialState()
     print(initial)
-    #print(generate_children(initial)) # Test generate children
+    # print(generate_children(initial)) # Test generate children
     print("BFS start with:\n", initial[0:3], "\n", initial[3:6], "\n", initial[6:], "\n")
     print(BFS(initial))
     print("DFS start with:\n", initial[0:3], "\n", initial[3:6], "\n", initial[6:], "\n")
